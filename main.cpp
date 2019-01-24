c4c3e80d1774a98cbc195480fbe8952307671f40
umaumax <umauma7083@gmail.com>
2019-01-24 19:34:29 +0900

    Add log files

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int add(int a, int b) {
+  //
+  return a + b;
+}
+
+float add(float a, float b) {
+  //
+  return a + b;
+}
+
+int add(int a, int b) {
+  //
+  return a + b;
+}
+
+int main(int argc, char* argv[]) {
+  add();
+  return 0;
+}
